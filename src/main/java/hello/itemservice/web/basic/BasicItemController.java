33b6b684ca4d97aeef53b2b9cb4bd1607bfa1f5d
pok3mon025 <pok3mon025@gmail.com>
2021-09-07 16:12:18 +0900

    상품 등록 처리 - @ModelAttribute

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -8,9 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
@@ -43,9 +45,45 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
+    // @PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+        @RequestParam int price,
+        @RequestParam Integer quantity,
+        Model model) {
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+    // @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item) {
+        itemRepository.save(item);
+        // model.addAttribute("item", item); //자동 추가, 생략 가능
+
+        return "basic/item";
+    }
+
+    // @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item) {
+        itemRepository.save(item);
+        // model.addAttribute("item", item); //클래스명 첫글자 소문자로 바꿔서 자동 추가, 생략 가능
+
+        return "basic/item";
+    }
+
     @PostMapping("/add")
-    public String save() {
-        return "basic/addForm";
+    public String addItemV4(Item item) {
+        itemRepository.save(item);
+        // model.addAttribute("item", item); //클래스명 첫글자 소문자로 바꿔서 자동 추가, 생략 가능
+
+        return "basic/item";
     }
 
     /**
